56cecf8d84a3e5f097fee80d76ca1c664302f41b
lucifer <azl397985856@gmail.com>
2021-07-09 16:51:49 +0800

    Update index.mjs

--- vue3-compiler/index.mjs
+++ vue3-compiler/index.mjs
@@ -1,10 +1,13 @@
 import vueDOM from "@vue/compiler-dom";
 import vueCore from "@vue/compiler-core";
 const { compile } = vueDOM;
-const { generate, transform } = vueCore;
-const { ast } = compile(`'<view bintap={{handleClick}} />'`);
+const { generate, transform, baseParse as parse } = vueCore;
+const transformations = [(node, context, option) => null]
+function compile(template) {
+  const ast = parse(template)
+  transform(ast, transformations)
+  return generate(ast)
+}
 
-const g = generate(ast);
-
-// transform(g.ast, {});
-console.log(g.ast, g.code);
+const { ast, code } = compile(`'<view bintap={{handleClick}} />'`);
+console.log(ast, code)
