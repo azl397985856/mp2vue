98cf483f82cc18c52ba2981acf0d5d184c507bf2
robot <xifa@tsign.cn>
2021-07-12 22:33:16 +0800

    feat: go

--- vue3-compiler/index.ts
+++ vue3-compiler/index.ts
@@ -6,10 +6,13 @@ import {
   transformElement,
   NodeTypes,
   RootNode,
-  transformBind,
+  DirectiveNode,
+  // transformOn,
+  // transformModel,
+  // transformBind,
   TemplateChildNode,
 } from "@vue/compiler-core";
-import template from "../vue2-compiler/index";
+// import template from "../vue2-compiler/index";
 
 const transformations = [
   transformElement,
@@ -17,6 +20,39 @@ const transformations = [
     if (node.type === NodeTypes.ROOT) {
     } else if (node.type === NodeTypes.ELEMENT) {
       node.tag = "bnc-" + node.tag;
+      for (const i in node.props) {
+        const attr = node.props[i];
+        if (attr.type === NodeTypes.ATTRIBUTE) {
+          const m = attr.value.content.match(/\{\{(.+)\}\}/);
+          if (m && m[1]) {
+            if (attr.name.startsWith("bind")) {
+              const dir: DirectiveNode = {
+                type: NodeTypes.DIRECTIVE,
+                modifiers: [],
+                name: "on",
+                exp: {
+                  type: NodeTypes.SIMPLE_EXPRESSION,
+                  isStatic: false,
+                  constType: 0,
+                  loc: attr.loc, // ??
+                  content: m[1],
+                },
+                arg: {
+                  content: `"${attr.name.slice(4)}"`,
+                  type: NodeTypes.SIMPLE_EXPRESSION,
+                  isStatic: false,
+                  constType: 0,
+                  loc: attr.loc, // ??
+                },
+                loc: attr.loc,
+              };
+              node.props[i] = dir;
+            } else {
+              attr.value.content = m[1];
+            }
+          }
+        }
+      }
     } else if (node.type === NodeTypes.TEXT) {
       node.content = "click you";
     } else {
@@ -30,12 +66,37 @@ function compile(template: string) {
     // prefixIdentifiers: true,
     nodeTransforms: transformations,
     directiveTransforms: {
-      bind: transformBind,
+      // once,
+      // for,
+      if(d, node) {
+        // console.log(d);
+        return {
+          props: [],
+        };
+      },
+      // on: (d, node) => {
+      //   // console.log(d, node);
+      //   return {
+      //     props: [],
+      //   };
+      // },
+      model: (d, node) => {
+        // console.log(d, node);
+        return {
+          props: [],
+        };
+      },
+      bind: (directive, node) => {
+        // console.log(d, node);
+        return {
+          props: [],
+        };
+      },
     },
   });
   return generate(ast);
 }
-// const template = `<view  @click="handleClick">click me</view>`;
+const template = `<view  bindtap={{handleClick}} type="primary" size={{config.size}}>click me</view>`;
 const { ast, code } = compile(template);
 
 console.log(code);
